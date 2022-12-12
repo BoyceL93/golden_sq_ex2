d81101950f28cdc39c043f119667bf733e00ebd5
Lauren Boyce <lauren.boyce93@gmail.com>
2022-12-12 16:23:55 +0000

    all outcomes dealt with

--- spec/check_codeword_spec.rb
+++ spec/check_codeword_spec.rb
@@ -1,7 +1,17 @@
 require 'check_codeword'
 
 RSpec.describe "check_codeword method" do
-    it "returns correct is codeword is horse, close if it starts/ends with h/e and wrong if not" do
+    it "returns wrong if given incorrect codeword" do
+        result = check_codeword('flangle')
+        expect(result).to eq "WRONG!"
+    end
+
+    it "returns close when keyword begins and ends with same letters as codeword" do
+        result = check_codeword('handle')
+        expect(result).to eq "Close, but nope."
+    end
+
+    it "returns correct if given correct codeword" do
         result = check_codeword('horse')
         expect(result).to eq "Correct! Come in."
     end
